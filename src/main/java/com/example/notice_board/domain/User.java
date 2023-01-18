8af1e8a3f29c89c9e3af870e33edd2e6cf21211e
sooyv <2cow0.0l@gmail.com>
2023-01-18 19:01:36 +0900

    유저 DB 저장 기능 구현

---
+++ src/main/java/com/example/notice_board/domain/User.java
@@ -0,0 +1,25 @@
+package com.example.notice_board.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter @Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(unique = true)  // nuique로 유일한 값만 저장할 수 있도록. 중복된 값 저장 불가
+    private String userNickName;
+
+    @Column
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+}
---
+++ src/main/java/com/example/notice_board/repositroy/UserRepostitory.java
@@ -0,0 +1,21 @@
+package com.example.notice_board.repositroy;
+
+import com.example.notice_board.domain.User;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class UserRepostitory {
+    @PersistenceContext
+    private EntityManager em;
+
+//    public UserRepostitory(EntityManager em) {
+//        this.em = em;
+//    }
+
+    public void save(User user) {
+        em.persist(user);
+    }
+}
